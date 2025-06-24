baca2ff8d23843b339393e09b88f1951cb209240
Chris Gomez <christiangomezdev@gmail.com>
2025-06-24 14:40:46 +0800

    feat: add throttling to chat controller and configure throttler module

--- backend/src/agent/chat/chat.controller.ts
+++ backend/src/agent/chat/chat.controller.ts
@@ -1,11 +1,18 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatResponseDto } from 'src/dtos/chatResponseDto';
+import { seconds, Throttle } from '@nestjs/throttler';
 
 @Controller('chat')
 export class ChatController {
     constructor(private readonly chatService: ChatService) { }
 
+    @Throttle({
+        default: {
+            ttl: seconds(60), // 1 minute
+            limit: 1 // 10 requests per minute
+        }
+    })
     @Post("")
     async chat(@Body() chatRequestDto: ChatRequestDto): Promise<ChatResponseDto> {
         const { message } = chatRequestDto;
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,15 +4,31 @@ import { AppService } from './app.service';
 import { AgentModule } from './agent/agent.module';
 import { LlmModule } from './llm/llm.module';
 import { ConfigModule } from '@nestjs/config';
+import { seconds, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-        isGlobal: true,
-        envFilePath: '.env',
-      }),
-    AgentModule, LlmModule],
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: seconds(60),
+          limit: 10,
+        }
+      ]
+    }),
+    AgentModule,
+    LlmModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: ThrottlerGuard,
+    },
+    AppService
+  ],
 })
 export class AppModule { }
